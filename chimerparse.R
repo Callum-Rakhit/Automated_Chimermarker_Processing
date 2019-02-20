9dfe9b64084051c0da09cd93ddec60e020003c51
Callum-Rakhit <callum_rakhit@hotmail.com>
2019-02-20 16:13:10 +0000

    Takes the exported excel, pattern matches and extracts key values.

---
+++ chimerparse.R
@@ -0,0 +1,95 @@
+# TODO(Callum)
+#   - Print the whole LMH/Sample type/date as string
+
+# Get args from the command line
+args = commandArgs(trailingOnly=TRUE)
+
+# Function to strip away empty cases
+CompleteFun <- function(data, desiredCols) {
+  completeVec <- complete.cases(data[, desiredCols])
+  return(data[completeVec, ])
+}
+
+# Function to check if packages are installed, if not, get them
+GetPackages <- function(required.packages) {
+  packages.not.installed <- 
+    required.packages[!(required.packages %in% installed.packages()[, "Package"])]
+  if(length(packages.not.installed)){
+    install.packages(packages.not.installed)}
+  lapply(required.packages, require, character.only = TRUE)
+}
+
+# Install/load required packages
+GetPackages(c("readxl", "stringr"))
+
+# Get the data
+if (length(args)==0) {
+  stop("Need to supply an input, e.g. Rscript chimerParser.R **input_file.xls**",
+       call.=FALSE)
+}
+
+data <- read_xlsx(args[1], col_names = F)
+names(data) = c("A", "B", "C", "D", "E", "F")
+
+# Create a dataset with no NAs in the first column
+data_no_NAs <- CompleteFun(data, 1)
+
+# Find the strings in the file
+bm_pattern <- "[:digit:][:digit:][:digit:][:digit:][:digit:][:digit:]-[A-Z][A-Z]_.............."
+cd3_pattern <- "[:digit:][:digit:][:digit:][:digit:][:digit:][:digit:]-[A-Z][A-Z][:digit:]_.............."
+cd15_pattern <- "[:digit:][:digit:][:digit:][:digit:][:digit:][:digit:]-[A-Z][A-Z][:digit:][:digit:]_.............."
+
+bm_string <- str_extract_all(data, bm_pattern, simplify = T)
+bm_string <- bm_string[1,]
+
+cd3_string <- str_extract_all(data, cd3_pattern, simplify = T)
+cd3_string <- cd3_string[1,]
+
+cd15_string <- str_extract_all(data, cd15_pattern, simplify = T)
+cd15_string <- cd15_string[1,]
+
+# Function to append df with LMH, sample type, date, chimerism (%) & loci
+output_data <- data.frame(matrix(
+  vector(), 0, 5, dimnames = list(c(), c(
+    "LMH_Number", "Sample_Type", "Date", "Average_Chimerism", "Informative_Loci"))),
+  stringsAsFactors=F)
+
+bigParser <- function(string_type) {
+  for (i in 1:length(string_type)){
+    # Extract the LMH/type/date name
+    temp_var <- string_type[i]
+    
+    # Split it up and extract LMH/type/gate
+    temp_var_split <- str_split(temp_var, "-", simplify = T)
+    temp_var_split <- temp_var_split[1,]
+    temp_var_LMH <- temp_var_split[1]
+    temp_var_type <- str_split(temp_var_split[2], "_", simplify = T)[1]
+    temp_var_date <- paste(str_split(temp_var_split[2], "_", simplify = T)[3],
+                           temp_var_split[3], temp_var_split[4], sep = "_")
+    
+    # Split the no NA df using the LMH/type/date name
+    data_no_NAs$tab_id <- cumsum(grepl(temp_var, data_no_NAs$A))
+    df_split <- split(data_no_NAs[, -ncol(data_no_NAs)], data_no_NAs$tab_id)
+    df_split <- as.data.frame(df_split[2])
+    
+    # From the split df, get chimerism percent and loci number
+    temp_chimerism <- df_split[2][20,]
+    temp_loci <- df_split[2][24,]
+    temp_output_string <- c(temp_var_LMH, temp_var_date, temp_var_type,
+                            temp_chimerism, temp_loci)
+    output_data[nrow(output_data) + 1,] <<- temp_output_string
+  }
+}
+
+# Run parser function across all patterns
+main <- function() {
+  bigParser(bm_string)
+  bigParser(cd15_string)
+  bigParser(cd3_string)
+  write.csv(x = output_data, file = args[2])
+}
+
+# If interactive R script, run main() function
+if (!interactive()) {
+  main()
+}
